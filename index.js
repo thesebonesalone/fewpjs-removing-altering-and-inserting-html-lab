3499f2f988886079a0b82d618e661470ad435467
thesebonesalone <duikhead2@gmail.com>
2020-10-26 13:46:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector("main").remove();
+let newHeader = document.createElement("H1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Will is the champion";
